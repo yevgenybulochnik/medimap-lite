59378cdeea8fa75bc17ed5813f63fdaf22c697c7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-10-28 20:10:58 -0700

    Initial commit

---
+++ precache-manifest.1da207e74da6f88e6fce2b312f03073a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be6d4cec8cc2c40854c2f2b65ad88d5",
+    "url": "/medimap-lite/index.html"
+  },
+  {
+    "revision": "7ee3694b8951d76ec590",
+    "url": "/medimap-lite/static/css/2.b922babc.chunk.css"
+  },
+  {
+    "revision": "f619afc91be96808a68f",
+    "url": "/medimap-lite/static/css/main.ed1198c7.chunk.css"
+  },
+  {
+    "revision": "7ee3694b8951d76ec590",
+    "url": "/medimap-lite/static/js/2.45dca819.chunk.js"
+  },
+  {
+    "revision": "d7521fac5a3b8512844c159e71f3b9c8",
+    "url": "/medimap-lite/static/js/2.45dca819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f619afc91be96808a68f",
+    "url": "/medimap-lite/static/js/main.630ace65.chunk.js"
+  },
+  {
+    "revision": "6b772a7057031b07afcb",
+    "url": "/medimap-lite/static/js/runtime-main.13cdaf5b.js"
+  }
+]);
